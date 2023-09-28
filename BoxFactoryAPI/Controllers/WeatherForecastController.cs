10cec59c8bc8d71568cc7eb09f79d0d661c252b7
Julia Ilasova <1julka1il@gmail.com>
2023-09-28 12:43:26 +0200

    initial commit, default project structure

---
+++ BoxFactoryAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoxFactoryAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
