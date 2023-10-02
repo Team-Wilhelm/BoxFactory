c45dcd09ded594345a18a6218641c6a345da830c
Julia Ilasova <1julka1il@gmail.com>
2023-10-02 10:40:37 +0200

    added DTOs and prepared the controller, service and repository CRUD calls, repository methods throw an exception

---
+++ Infrastructure/BoxRepository.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+
+namespace Infrastructure;
+
+public class BoxRepository
+{
+    public async Task<IEnumerable<Box>> Get()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<Box> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<Box> Create(BoxCreateDto boxCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<Box> Update(Guid id, BoxUpdateDto boxUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
