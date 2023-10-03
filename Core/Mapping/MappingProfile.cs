873326764e755255bc86ef94cee8a2e88a7861f6
Julia Ilasova <1julka1il@gmail.com>
2023-10-03 15:45:59 +0200

    Updates to create and update a box, deleting added. I created a DimensionsDto.cs class, which does not require a uuid and it's no longer possible to change the dimensions uuid, only the values belonging to a box's assigned dimensions id

--- Core/Mapping/MappingProfile.cs
+++ Core/Mapping/MappingProfile.cs
@@ -8,7 +8,27 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<BoxCreateDto, Box>()
+            .ForMember(dest => dest.Dimensions,
+                opt => opt
+                    .MapFrom(src => new Dimensions
+                    {
+                        Length = src.DimensionsDto.Length,
+                        Width = src.DimensionsDto.Width,
+                        Height = src.DimensionsDto.Height
+                    }))
             .ReverseMap();
-    }   
-    
+
+        CreateMap<BoxUpdateDto, Box>()
+            .ForMember(dest => dest.Dimensions,
+                opt => opt
+                    .MapFrom(src => new Dimensions
+                    {
+                        Length = src.DimensionsDto.Length,
+                        Width = src.DimensionsDto.Width,
+                        Height = src.DimensionsDto.Height
+                    }))
+            .ReverseMap();
+
+        CreateMap<Dimensions, DimensionsDto>();
+    }  
 }
--- Core/Services/BoxService.cs
+++ Core/Services/BoxService.cs
@@ -19,24 +19,31 @@ public class BoxService
     {
         return await _boxRepository.Get();
     }
-    
+
     public async Task<Box> Get(Guid id)
     {
         return await _boxRepository.Get(id);
     }
-    
+
     public async Task<Box> Create(BoxCreateDto boxCreateDto)
     {
         var box = _mapper.Map<Box>(boxCreateDto);
         box.CreatedAt = DateTime.Now;
         return await _boxRepository.Create(box);
     }
-    
+
     public async Task<Box> Update(Guid id, BoxUpdateDto boxUpdateDto)
     {
-        return await _boxRepository.Update(id, boxUpdateDto);
+        var box = await _boxRepository.Get(id);
+        if (box == null)
+        {
+            throw new Exception("Box not found");
+        }
+
+        box = _mapper.Map(boxUpdateDto, box);
+        return await _boxRepository.Update(box);
     }
-    
+
     public async Task Delete(Guid id)
     {
         await _boxRepository.Delete(id);
