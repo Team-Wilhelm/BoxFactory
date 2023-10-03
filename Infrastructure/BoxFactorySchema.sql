91264083f8b2c1d1346458689aac8479e6de45ff
Julia Ilasova <1julka1il@gmail.com>
2023-10-03 10:17:23 +0200

    updated database script

--- Infrastructure/BoxFactorySchema.sql
+++ Infrastructure/BoxFactorySchema.sql
@@ -1,36 +1,27 @@
-DROP SCHEMA IF EXISTS "testing" CASCADE;
-CREATE SCHEMA "testing";
+DROP SCHEMA IF EXISTS testing CASCADE;
+CREATE SCHEMA testing;
 
-CREATE TABLE IF NOT EXISTS "Boxes"
+CREATE TABLE IF NOT EXISTS testing.Dimensions
 (
-    "box_id"        uuid PRIMARY KEY,
-    "weight"        integer,
-    "colour"        varchar(25),
-    "material"      varchar(25),
-    "price"         float,
-    "created_at"    timestamp,
-    "dimensions_id" uuid REFERENCES "Dimensions" ("dimensions_id") ON DELETE CASCADE
-);
-
-CREATE TABLE "Dimensions"
-(
-    "dimensions_id" integer PRIMARY KEY,
+    "dimensions_id" uuid PRIMARY KEY,
     "length"        float,
     "width"         float,
     "height"        float
 );
 
-CREATE TABLE "Orders"
+CREATE TABLE IF NOT EXISTS testing.Boxes
 (
-    "order_id"    uuid PRIMARY KEY,
-    "status"      varchar(25),
-    "created_at"  timestamp,
-    "updated_at"  timestamp,
-    "customer_id" uuid REFERENCES "Customers" ("customer_id"),
-    "address_id"  uuid REFERENCES "Addresses" ("address_id")
+    "box_id"        uuid PRIMARY KEY,
+    "weight"        float,
+    "colour"        varchar(25),
+    "material"      varchar(25),
+    "price"         float,
+    "created_at"    timestamp,
+    "stock"         integer,
+    "dimensions_id" uuid REFERENCES testing.Dimensions ("dimensions_id") ON DELETE CASCADE
 );
 
-CREATE TABLE "Customers"
+CREATE TABLE IF NOT EXISTS testing.Customers
 (
     "customer_id"  uuid PRIMARY KEY,
     "email"        varchar,
@@ -39,7 +30,7 @@ CREATE TABLE "Customers"
     "last_name"    varchar
 );
 
-CREATE TABLE "Addresses"
+CREATE TABLE IF NOT EXISTS testing.Addresses
 (
     "address_id"            uuid PRIMARY KEY,
     "street_name"           varchar,
@@ -47,41 +38,36 @@ CREATE TABLE "Addresses"
     "house_number_addition" varchar(10),
     "city"                  varchar,
     "postal_code"           varchar(10),
-    "country"               varchar
+    "country"               varchar(25)
 );
 
-CREATE TABLE "Stock"
+CREATE TABLE IF NOT EXISTS testing.Orders
 (
-    "stock_id" uuid PRIMARY KEY,
-    "quantity" int
+    "order_id"    uuid PRIMARY KEY,
+    "status"      varchar(25),
+    "created_at"  timestamp,
+    "updated_at"  timestamp,
+    "customer_id" uuid REFERENCES testing.Customers ("customer_id"),
+    "address_id"  uuid REFERENCES testing.Addresses ("address_id")
 );
 
-CREATE TABLE "Box_Order_Link"
+CREATE TABLE IF NOT EXISTS testing.Box_Order_Link
 (
     PRIMARY KEY (
         "box_id",
         "order_id"
     ),
-    "box_id"   uuid REFERENCES "Boxes" ("box_id"),
-    "order_id" uuid REFERENCES "Orders" ("order_id")
+    "box_id"   uuid REFERENCES testing.Boxes ("box_id"),
+    "order_id" uuid REFERENCES testing.Orders ("order_id"),
+    "quantity" integer
 );
 
-CREATE TABLE "Box_Stock_Link"
-(
-    PRIMARY KEY (
-        "box_id",
-        "stock_id"
-    ),
-    "box_id"   uuid REFERENCES "Boxes" ("box_id"), 
-    "stock_id" uuid REFERENCES "Stock" ("stock_id")
-);
-
-CREATE TABLE "Customer_Address_Link"
+CREATE TABLE IF NOT EXISTS testing.Customer_Address_Link
 (
     PRIMARY KEY (
         "customer_id",
         "address_id"
     ),
-    "customer_id" uuid REFERENCES "Customers" ("customer_id"),
-    "address_id"  uuid REFERENCES "Addresses" ("address_id")
+    "customer_id" uuid REFERENCES testing.Customers ("customer_id"),
+    "address_id"  uuid REFERENCES testing.Addresses ("address_id")
 );
