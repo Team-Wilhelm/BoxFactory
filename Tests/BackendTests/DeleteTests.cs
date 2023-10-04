8ce1d5752aa9a64d9d5a8ae192106dfd04d652d4
Julia Ilasova <1julka1il@gmail.com>
2023-10-04 12:08:01 +0200

    Get and Update tests now work, separated out some creational logic into the Helper class.

--- Tests/BackendTests/DeleteTests.cs
+++ Tests/BackendTests/DeleteTests.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
 using FluentAssertions;
 using FluentAssertions.Execution;
-using Models;
 
 namespace Tests.BackendTests;
 
@@ -22,38 +21,7 @@ public class DeleteTests
         // Arrange
         Helper.TriggerRebuild();
         var boxDto = Helper.CreateBoxCreateDto(weight, colour, material, price, 1, height, length, width);
-        
-        // Insert dimensions
-        var sql = $@"INSERT INTO testing.dimensions (length, width, height) 
-                        VALUES (@Length, @Width, @Height) 
-                        RETURNING 
-                            dimensions_id AS {nameof(Dimensions.Id)},
-                            length AS {nameof(Dimensions.Length)},
-                            height AS {nameof(Dimensions.Height)},
-                            width AS {nameof(Dimensions.Width)}";
-        var dimensions = await Helper.DbConnection.QuerySingleAsync<Dimensions>(sql, boxDto.DimensionsDto);
-
-        // Insert the box and link it to the dimensions, set the box's dimensions to the dimensions object
-        sql = @$"INSERT INTO testing.boxes (weight, colour, material, price, created_at, stock, dimensions_id) 
-                    VALUES (@Weight, @Colour, @Material, @Price, NOW(), @Stock, @DimensionsId)
-                    RETURNING
-                        weight AS {nameof(Box.Weight)},
-                        colour AS {nameof(Box.Colour)},
-                        material AS {nameof(Box.Material)},
-                        price AS {nameof(Box.Price)},
-                        created_at AS {nameof(Box.CreatedAt)},
-                        stock AS {nameof(Box.Stock)},
-                        box_id AS {nameof(Box.Id)};";
-        var box = await Helper.DbConnection.QuerySingleAsync<Box>(sql, new
-        {
-            boxDto.Weight,
-            boxDto.Colour,
-            boxDto.Material,
-            boxDto.Price,
-            boxDto.Stock,
-            DimensionsId = dimensions.Id
-        });
-        box.Dimensions = dimensions;
+        var box = await Helper.InsertBoxIntoDatabase(boxDto);
         
         // Act
         var url = Helper.UrlBase + $"/box/{box.Id}";
@@ -71,9 +39,9 @@ public class DeleteTests
         // Assert
         using (new AssertionScope())
         {
-            sql = $"SELECT * FROM testing.boxes WHERE box_id = '{box.Id}';";
-            await Helper.DbConnection.ExecuteAsync(sql, box.Id);
             response.IsSuccessStatusCode.Should().BeTrue();
+            var boxAmount = await Helper.DbConnection.ExecuteScalarAsync<int>("SELECT * FROM testing.boxes WHERE box_id = @Id", new { box.Id });
+            boxAmount.Should().Be(0);
         }
     }
 }
