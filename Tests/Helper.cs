cdbdee610984a09fa2d3401762e98b04b1814c8e
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2023-10-08 17:04:46 +0200

    Updated scripts to reflect customer email being the primary key

--- Tests/Helper.cs
+++ Tests/Helper.cs
@@ -117,8 +117,7 @@ CREATE TABLE IF NOT EXISTS testing.Boxes
 
 CREATE TABLE IF NOT EXISTS testing.Customers
 (
-    customer_id  uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email        varchar,
+    customer_email        varchar PRIMARY KEY,
     phone_number varchar(20),
     first_name   varchar(25),
     last_name    varchar
@@ -141,7 +140,7 @@ CREATE TABLE IF NOT EXISTS testing.Orders
     status      varchar(25),
     created_at  timestamp,
     updated_at  timestamp,
-    customer_id uuid REFERENCES testing.Customers (customer_id),
+    customer_email varchar REFERENCES testing.Customers (customer_email),
     address_id  uuid REFERENCES testing.Addresses (address_id)
 );
 
@@ -159,10 +158,10 @@ CREATE TABLE IF NOT EXISTS testing.Box_Order_Link
 CREATE TABLE IF NOT EXISTS testing.Customer_Address_Link
 (
     PRIMARY KEY (
-        customer_id,
+        customer_email,
         address_id
     ),
-    customer_id uuid REFERENCES testing.Customers (customer_id),
+    customer_email uuid REFERENCES testing.Customers (customer_email),
     address_id  uuid REFERENCES testing.Addresses (address_id)
 );
  ";
