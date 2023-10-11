35b486547972de64cda1e4b85cdf7208527efb4b
Julia Ilasova <1julka1il@gmail.com>
2023-10-11 19:33:24 +0200

    get stats endpoint now returns data properly

--- Infrastructure/StatsRepository.cs
+++ Infrastructure/StatsRepository.cs
@@ -21,18 +21,33 @@ public class StatsRepository
     {
         var statsSql = @$"
     SELECT
-        EXTRACT(MONTH FROM o.created_at) AS Month,
-        SUM(bol.quantity) AS Boxes
+        EXTRACT(MONTH FROM o.created_at)::integer AS Month,
+        SUM(bol.quantity)::integer AS Boxes
     FROM {_databaseSchema}.orders o 
     INNER JOIN {_databaseSchema}.box_order_link bol ON o.order_id = bol.order_id 
     WHERE o.created_at > NOW() - INTERVAL '12 months'
     GROUP BY EXTRACT(MONTH FROM o.created_at)
     ORDER BY EXTRACT(MONTH FROM o.created_at);
 ";
-
-        var statsList = await _dbConnection.QueryAsync(statsSql);
-
-        return new StatsDto { OrdersPerMonth = statsList.ToDictionary(x => (int)x.Month, x => (int)x.Boxes) };
-
+        var statsList = (await _dbConnection.QueryAsync(statsSql)).ToList();
+        
+        // The frontend expects the month to be 0-indexed, so we subtract 1 from the month
+        // Also, we need to convert the IEnumerable<dynamic> to a Dictionary<int, int>
+        // and put 0 for months that have no orders
+        var statsDictionary = new Dictionary<int, int>();
+        for (int i = 0; i < 12; i++)
+        {
+            statsDictionary.Add(i, 0);
+        }
+        
+        for (int i = 0; i < statsList.Count; i++)
+        {
+            var statsListMonth = statsList[i].month - 1;
+            statsDictionary[statsListMonth] = statsList[i].boxes;
+        }
+        return new StatsDto
+        {
+            OrdersPerMonth = statsDictionary
+        };
     }
 }
